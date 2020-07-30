97ff2bd8dc7f73e2d424505598f165f911375060
Sebastien Rothhut <67049538+sebastien-rothhut@users.noreply.github.com>
2020-07-30 17:04:19 +0200

    Feature/ces 1410 cli operational tooling (#62)
    * CES-1410 use picocli, package with ZIP layout for Spring Boot CLI
    
    * CES-1410 add picocli as direct dependency
    
    * CES-1410 Gather all CLI in admin tools

--- sfs-ms-chat-gateway-server/init-db.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-export JAVA_PROGRAM_ARGS=`echo "$@"`
-mvn -q clean compile exec:java \
-  -Dexec.mainClass="com.symphony.sfs.ms.chat.InitSfsChatGatewayDB" \
-  -Dexec.args="$JAVA_PROGRAM_ARGS"
