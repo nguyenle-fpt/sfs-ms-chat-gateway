4fa5e1218e719e2dcb293a4142ba2f7ed19a7175
Fabien Vicente <64403764+FabienVSymphony@users.noreply.github.com>
2020-08-05 15:52:30 +0200

    Merge ITF sfs into master (#65)
    Co-authored-by: Benjamin Billet <benjamin.billet@symphony.com>
    Co-authored-by: syachraf <achraf.elamrani@symphony.com>
    Co-authored-by: cedricmorandin <67632033+cedricmorandin@users.noreply.github.com>
    Co-authored-by: Nicolas Ribaud <nicolas.ribaud@symphony.com>

--- install-m2.sh
+++ install-m2.sh
@@ -2,3 +2,4 @@
 
 mvn -f pom.xml clean install -DskipTests --non-recursive
 mvn -f sfs-ms-chat-gateway-client/pom.xml clean install -DskipTests
+mvn -f sfs-ms-chat-gateway-dal/pom.xml clean install -DskipTests
